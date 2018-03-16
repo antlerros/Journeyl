8f6617fd1df32fda3408facac2132a666890a688
PingHsuan Tsai <b02502094@ntu.edu.tw>
2018-03-16 16:24:41 -0500

    Clean up

--- Journeyl/app/src/main/java/com/example/antlerros95/journeyl/MainActivity.java
+++ Journeyl/app/src/main/java/com/example/antlerros95/journeyl/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addGoalButton = (Button) findViewById(R.id.buttonAddGoal);
+        addGoalButton = findViewById(R.id.buttonAddGoal);
 
         addGoalButton.setOnClickListener(new View.OnClickListener() {
             @Override

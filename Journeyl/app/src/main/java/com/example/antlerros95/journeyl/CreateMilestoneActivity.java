3d0472c745d17fe9565857c292558672e6a63898
PingHsuan Tsai <b02502094@ntu.edu.tw>
2018-03-16 16:24:09 -0500

    Remove *.xmls; Create Activities

---
+++ Journeyl/app/src/main/java/com/example/antlerros95/journeyl/CreateMilestoneActivity.java
@@ -0,0 +1,13 @@
+package com.example.antlerros95.journeyl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMilestoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_milestone);
+    }
+}
--- Journeyl/app/src/main/java/com/example/antlerros95/journeyl/MainActivity.java
+++ Journeyl/app/src/main/java/com/example/antlerros95/journeyl/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.antlerros95.journeyl;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addGoalButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addGoalButton = (Button) findViewById(R.id.buttonAddGoal);
+
+        addGoalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CreateGoalActivity.class));
+            }
+        });
     }
 
 //    @Override

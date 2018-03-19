7510af6693b4b104ae2f624548262f0fc9ba5f9d
Leyton Tsai <b02502094@ntu.edu.tw>
2018-03-19 12:55:02 -0500

    Create MilestoneDetail Activity

---
+++ Journeyl/app/src/main/java/com/example/antlerros95/journeyl/MilestoneDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.antlerros95.journeyl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MilestoneDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_milestone_detail);
+    }
+}

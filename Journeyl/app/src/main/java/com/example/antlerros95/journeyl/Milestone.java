b8a2b69a9aa8d0cfb9765f8f3288f485abe1f0ef
Leyton Tsai <b02502094@ntu.edu.tw>
2018-03-20 09:33:02 -0500

    Add fields to Milestone object

--- Journeyl/app/src/main/java/com/example/antlerros95/journeyl/Milestone.java
+++ Journeyl/app/src/main/java/com/example/antlerros95/journeyl/Milestone.java
@@ -7,18 +7,34 @@ package com.example.antlerros95.journeyl;
 public class Milestone extends Item {
 
     private String mDescription;
+    private int mID;
 
-    // TODO: Should be a series of values
+    // TODO: Should be a series of values?
     private float ratings;
+    private String mReflection;
 
 
     public Milestone(String name) {
         super(name);
     }
 
-    public Milestone(String name, String description) {
+    public Milestone(String name, String description, int mID) {
         super(name);
         this.mDescription = description;
+        this.mID = mID;
     }
 
+    public int getID() { return mID; }
+
+    public void setID(int ID) {
+        this.mID = ID;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setReflection(String reflection) {
+        this.mReflection = reflection;
+    }
 }

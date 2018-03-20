2ecef95e9aea562f2fdf915e31992a225e973395
Leyton Tsai <phtsai2@illinois.edu>
2018-03-20 11:01:48 -0500

    Update Reflection Activity

--- Journeyl/app/src/main/java/com/example/antlerros95/journeyl/ReflectActivity.java
+++ Journeyl/app/src/main/java/com/example/antlerros95/journeyl/ReflectActivity.java
@@ -3,11 +3,17 @@ package com.example.antlerros95.journeyl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ms.square.android.expandabletextview.ExpandableTextView;
+
 public class ReflectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflect);
+
+        ExpandableTextView descriptionExpandable = (ExpandableTextView) findViewById(R.id.expand_text_view);
+
+        descriptionExpandable.setText("Some description");
     }
 }
